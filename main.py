f0aa8ba3167dcc584b0ab83080618461a913cafa
User <nizamov.dinislam.04@mail.ru>
2021-02-15 20:55:55 +0500

    Не работает

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.dialog = addEditCoffeeForm(self.x)
+        self.dialog = addEditCoffeeForm()
         uic.loadUi('main.ui', self)
-        con = sqlite3.connect("coffee.sqlite")
-        cur = con.cursor()
-        self.result = cur.execute("""SELECT * FROM coffee""").fetchall()
+        self.con = sqlite3.connect("coffee.sqlite")
+        self.cur = self.con.cursor()
+        self.result = self.cur.execute("""SELECT * FROM coffee""").fetchall()
         names = [i[1] for i in self.result]
         self.comboBox.setPlaceholderText("Выберете вид кофе")
         self.pushButton.clicked.connect(self.ShowSecondForm)
@@ -38,7 +38,6 @@ class MyWidget(QMainWindow):
     def ShowSecondForm(self):
         self.dialog.show()
 
-
     def on_combobox_func(self):
         name = self.comboBox.currentText()
         self.id_label.show()
@@ -53,6 +52,7 @@ class MyWidget(QMainWindow):
         self.label_5.hide()
         self.pushButton.show()
         self.pushButton_2.show()
+
         for i in self.result:
             if name == i[1]:
                 self.id_label.setText(f'  Id: {i[0]}')
@@ -62,21 +62,46 @@ class MyWidget(QMainWindow):
                 self.textBrowser.setPlainText('    ' + i[4])
                 self.price_label.setText(f'  Цена: {i[5]}')
                 self.o_label.setText(f'  Объем упаковки: {i[6]}')
-                self.x = i[0]
+                # self.dialog.lineEdit(f'  Id: {i[0]}')
+                self.dialog.lnd(i)
                 break
 
 
 class addEditCoffeeForm(QWidget):
-    def __init__(self, main):
+    def __init__(self):
         super().__init__()
         uic.loadUi('addEditCoffeeForm.ui', self)
+        self.pushButtond.clicked.connect(lambda x: self.close())
+        self.pushButton_2d.clicked.connect(self.save)
 
-        self.lineEdit.setText(main)
-        self.lineEdit_2.setText()
-        self.lineEdit_3.setText()
-        self.lineEdit_4.setText()
-        self.lineEdit_5.setText()
-        self.lineEdit_6.setText()
+    def save(self):
+        new_list = []
+        con = sqlite3.connect("coffee.sqlite")
+        cur = con.cursor()
+        text = self.lineEditd.text()
+        new_list.append(self.lineEdit_2d.text())
+        new_list.append(self.lineEdit_3d.text())
+        new_list.append(self.lineEdit_4d.text())
+        new_list.append(self.textBrowserd.toPlainText())
+        new_list.append(self.lineEdit_5d.text())
+        new_list.append(self.lineEdit_6d.text())
+        new_list.append(self.lineEditd.text())
+        if not self.isHidden():
+            cur.execute(
+                """UPDATE coffee SET Name = ? AND obz = ? AND zer = ? AND text = ? AND price = 
+                ? AND ob = ? WHERE id = ?""", new_list)
+            con.commit()
+            self.close()
+
+    def lnd(self, k):
+        self.lineEditd.setText(str(k[0]))
+        self.lineEdit_2d.setText(str(k[1]))
+        self.lineEdit_3d.setText(str(k[2]))
+        self.lineEdit_4d.setText(str(k[3]))
+        self.textBrowserd.setPlainText('    ' + k[4])
+        self.lineEdit_5d.setText(str(k[5]))
+        self.lineEdit_6d.setText(str(k[6]))
+
 
 
 if __name__ == '__main__':
